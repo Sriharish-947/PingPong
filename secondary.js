8ee70b55cb09ebc1a990f7146c9cc7c2e3bac5d0
Sriharish-947 <74801176+Sriharish-947@users.noreply.github.com>
2022-05-06 20:24:20 -0400

    Add files via upload

---
+++ secondary.js
@@ -0,0 +1,25 @@
+/*
+Status = " ";
+Objects = [ ];
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw()
+{
+    if(Status == " ")
+    {
+        
+    }
+}
+*/
